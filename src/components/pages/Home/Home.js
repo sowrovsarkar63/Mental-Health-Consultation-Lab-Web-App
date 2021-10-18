70b49b66d6eb23304ab028d324b08a5ebf837898
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-18 22:31:37 +0600

    using reactbootstrap for faster development

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Button, Container } from "react-bootstrap";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home page</h1>
-        </div>
+        <Container>
+            <h1>This is home </h1>
+            <Button variant="primary" size="sm">
+                Large button
+            </Button>
+        </Container>
     );
 };
 
