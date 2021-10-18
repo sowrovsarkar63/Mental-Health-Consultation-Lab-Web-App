b6333a15920147eb6bbec910e52b8aba9836dc4b
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-19 00:55:31 +0600

    website name updated

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
 
 const Home = () => {
     return (
-        <Container>
-            <h1>This is home </h1>
-            <Button variant="primary" size="sm">
-                Large button
-            </Button>
-        </Container>
+        <>
+            <h1>something</h1>
+        </>
     );
 };
 
