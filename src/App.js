bf2c9a164742da2ddc00467e8e5909b72a9d1997
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-18 21:56:12 +0600

    React router is working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./components/pages/Home/Home";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <Home></Home>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
